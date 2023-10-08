e83f13455412009988d2efd6eac7ef2facd107ea
wulonmt <zuck0087285@gmail.com>
2023-10-08 17:59:51 +0800

    modify Ptime get_orign_time & fix my make_vec_env

--- Ptime.py
+++ Ptime.py
@@ -21,9 +21,10 @@ class Ptime():
         }
     def set_time_now(self):
         self.saved_time = str(time.ctime())
+        self.saved_origin = int(time.time())
         
     def get_origin_time(self):
-        return self.saved_time
+        return self.saved_origin
         
     def get_time(self):
         time_list = self.saved_time.split(' ')
@@ -32,6 +33,7 @@ class Ptime():
         if(int(time_list[2]) < 10):
             time_list[2] = "0" + time_list[2]
         time_list[1] = self.month_number_dict[time_list[1]]
+        time_list[3] = time_list[3].replace(":",".")
         mask = [4, 1, 2, 3]
         self.ptime = ""
         for i in mask:
@@ -41,4 +43,5 @@ class Ptime():
 if __name__ == "__main__":
     t = Ptime()
     t.set_time_now()
-    print("time now:: ", t.get_time())
+    print("time now: ", t.get_time())
+    print("time origin: ", t.get_origin_time())
