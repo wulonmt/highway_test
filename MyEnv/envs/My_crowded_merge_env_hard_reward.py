3d725c6010289fa016192ddc86c02fd049821087
wulonmt <zuck0087285@gmail.com>
2024-01-20 10:32:49 +0800

    add merge hard

---
+++ MyEnv/envs/My_crowded_merge_env_hard_reward.py
@@ -0,0 +1,65 @@
+from typing import Dict, Text
+
+import numpy as np
+
+from highway_env import utils
+from highway_env.envs.merge_env import MergeEnv
+from highway_env.road.lane import LineType, StraightLane, SineLane
+from highway_env.road.road import Road, RoadNetwork
+from highway_env.vehicle.controller import ControlledVehicle
+from highway_env.vehicle.objects import Obstacle
+
+class MyCrowdedMergeEnvHardReward(MergeEnv):
+    @classmethod
+    def default_config(cls) -> dict:
+        cfg = super().default_config()
+        cfg.update(
+            {"observation": {
+                       "type": "GrayscaleObservation",
+                       "observation_shape": (84, 84),
+                       "stack_size": 4,
+                       "weights": [0.9, 0.1, 0.5],  # weights for RGB conversion
+                       "scaling": 1.75,
+                   },
+            "collision_reward": -1,
+            "right_lane_reward": 0.1,
+            "high_speed_reward": 0.4,
+            "merging_speed_reward": -0.5,
+            "lane_change_reward": 0,
+            "vehicles_density": 2,
+            "vehicles_count": 10,
+            })
+        return cfg
+        
+    def _make_vehicles(self) -> None:
+        """
+        Populate a road with several vehicles on the highway and on the merging lane, as well as an ego-vehicle.
+
+        :return: the ego-vehicle
+        """
+        road = self.road
+        ego_vehicle = self.action_type.vehicle_class(road,
+                                                     road.network.get_lane(("a", "b", 1)).position(30, 0),
+                                                     speed=30)
+        road.vehicles.append(ego_vehicle)
+
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+        for _ in range(self.config["vehicles_count"]):
+            vehicle = other_vehicles_type.create_random(road, spacing=1 / self.config["vehicles_density"])
+            vehicle.randomize_behavior()
+            self.road.vehicles.append(vehicle)
+        """
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(90, 0), speed=29))
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 1)).position(70, 0), speed=31))
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(5, 0), speed=31.5))
+        """
+        merging_v = other_vehicles_type(road, road.network.get_lane(("j", "k", 0)).position(110, 0), speed=20)
+        merging_v.target_speed = 30
+        road.vehicles.append(merging_v)
+        self.vehicle = ego_vehicle
+    
+    def _reward(self, action: int) -> float:
+        if self.vehicle.position[0] > 370:
+            return 1
+        else:
+            return 0
